484015a8198c18714f76f2dedb6ea504d9e0853f
webQbe <webcubesolutionslk@gmail.com>
2024-12-22 18:21:09 +0530

    Defined search url & result count

---
+++ settings.py
@@ -0,0 +1,12 @@
+SEARCH_KEY = "YOUR API KEY"
+SEARCH_ID = "ID OF PROGRAMMABLE SEARCH ENGINE"
+COUNTRY = "lk"
+
+''' Create URL to Call Get Search Results 
+    by Pass-in API Key, ID, Query, Result Page, & Country'''
+SEARCH_URL = "https://https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+
+''' Define Result Count Per Search '''
+RESULT_COUNT = 20
+
+
