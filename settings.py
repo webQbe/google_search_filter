45a3502f4c131c0dd4780a514d4edc805b707a61
webQbe <webcubesolutionslk@gmail.com>
2024-12-23 11:57:32 +0530

    Fixed search url error

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ COUNTRY = "lk"
 
 ''' Create URL to Call Get Search Results 
     by Pass-in API Key, ID, Query, Result Page, & Country'''
-SEARCH_URL = "https://https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
+SEARCH_URL = "https://www.googleapis.com/customsearch/v1?key={key}&cx={cx}&q={query}&start={start}&gl=" + COUNTRY
 
 ''' Define Result Count Per Search '''
 RESULT_COUNT = 20
