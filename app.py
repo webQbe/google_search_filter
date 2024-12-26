5d3a289e71413f19ef08ec5a55b5357ddd57102a
webQbe <webcubesolutionslk@gmail.com>
2024-12-26 19:34:47 +0530

    Updated run_search()

--- app.py
+++ app.py
@@ -27,6 +27,22 @@ def show_search_form():
 def run_search(query):
     results = search(query)
     rendered = search_template
+    
+    # Avoid rendering html included in snippets
+    results["snippet"] = results["snippet"].apply(lambda x: html.escape(x))
+
+    # Iterate across rows in results
+    for index, row in results.iterrows():
+        rendered += result_template.format(**row) # Append to rendered
+        ''' Pass current row of data to the template, 
+             so placeholder text in html will be replaced.
+        '''
+    # Return search results page
+    return rendered
+
+
+
+
 
 # Create a new route (127.0.0.1:5001)
 # for GET & POST requests 
