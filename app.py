654d065e25f9741d783277bda1bf95e1cc4c8236
webQbe <webcubesolutionslk@gmail.com>
2024-12-26 19:04:23 +0530

    Created new route for search_form()

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from search import search # Import Search() we created
+import html # To render html
+
+# Init Flask app
+app = Flask(__name__)
+
+# Create a new route (127.0.0.1:5001)
+# for GET & POST requests 
+@app.route("/", methods=["GET", "POST"]) # URL on web server 
+def search_form():
+    # Search for something if it's a POST request
+    if request.method == "POST":
+        # Get query from request data 
+        query = request.form["query"] 
+        return run_search(query)
+    else:
+        return show_search_form()
+    
+
+
+
+
