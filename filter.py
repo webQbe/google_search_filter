b29f93f7c7ff9be062b6d9b007ef8ca0ad2e2a66
webQbe <webcubesolutionslk@gmail.com>
2024-12-27 11:59:52 +0530

    Updated content_filter()

--- filter.py
+++ filter.py
@@ -1,11 +1,14 @@
 ''' Filtering & Re-ranking results '''
+
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from settings import *
 
 # Get text inside html
 def get_page_content(row):
+    # Parse the HTML content in the "html" column.
     soup = BeautifulSoup(row["html"])
+     # Extract all visible text from the HTML
     text = soup.get_text()
     return text
 
@@ -24,3 +27,23 @@ class Filter():
     def content_filter(self):
         # Apply get_page_content() to each row of filtered df  
         page_content = self.filtered.apply(get_page_content, axis=1)
+        
+        # Split extracted text (x) into words & calculate word count
+        word_count = page_content.apply(lambda x: len(x.split("")))
+
+        # Normalize word count by 
+        # dividing it by the median word count across all results.
+        word_count /= word_count.median()
+
+        # Assign RESULT_COUNT value to results where the normalized word count is less than or equal to 0.5
+        # Pages with a low word count (below half the median) are penalized by increasing their rank (bad results have higher rank values)
+        word_count[word_count <= .5] = RESULT_COUNT 
+
+        # For all other results, sets the value to 0
+        # Pages with a higher word count are not penalized
+        word_count[word_count != RESULT_COUNT] = 0
+
+        # Modify the existing rank column in the filtered DataFrame by adding the adjusted word count values.
+        self.filtered["rank"] += word_count
+
+
